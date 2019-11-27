a848e5a488410af32cb483492d899b533a0b0859
Tim Glaser <tim.glaser@hiberly.com>
2019-11-27 16:44:16 +0000

    Use GitLab url to redirect

--- hiberly_gateway/gitlab.py
+++ hiberly_gateway/gitlab.py
@@ -1,4 +1,6 @@
 from social_core.backends.gitlab import GitLabOAuth2 as SocialGitLabOAuth2 # type: ignore
+from django.conf import settings
     
 class GitLabOAuth2(SocialGitLabOAuth2):
     DEFAULT_SCOPE = ['api']
+    API_URL = settings.GITLAB_URL
